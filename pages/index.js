ee35d39749729274d8badb1e10a905ce85295f20
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2022-09-05 22:51:40 -0500

    add: form

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
-function HomePage() {
+import { useRef } from "react";
+
+const HomePage = () => {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  const submitFormHandler = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
-}
+};
 
 export default HomePage;
